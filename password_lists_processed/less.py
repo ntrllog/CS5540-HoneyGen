2cbabbb6ce7295aef0fb4e7658d0459be559bfcb
ntrllog <ntrllog@gmail.com>
2022-07-11 21:48:09 -0700

    add 3quarter password list

--- password_lists_processed/less.py
+++ password_lists_processed/less.py
@@ -1,3 +1,10 @@
+'''
+i % 2 == 0 for half
+i % 3 == 0 for third
+i % 4 == 0 for quarter
+i % 4 != 0 for 3quarter
+'''
+
 with open('rockyou_sorted_preprocessed.txt', encoding='utf-8') as f:
     lines = f.readlines()
 
